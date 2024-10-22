c29f8f9fe63326428361f2dc28807a7ad6014282
gregorygromov <gregorygromow@icloud.com>
2024-10-22 19:26:07 +0300

    refactoring

--- scripts/test1.ts
+++ scripts/test1.ts
@@ -1,18 +1,3 @@
-// const hre = require("hardhat");
-
-// async function main() {
-//   const accounts = await hre.ethers.getSigners();
-
-//   for (const account of accounts) {
-//     console.log(account.address);
-//   }
-// }
-
-// main().catch((error) => {
-//   console.error(error);
-//   process.exitCode = 1;
-// });
-
 import { ethers } from "hardhat";
 
 async function main() {
